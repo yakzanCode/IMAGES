16b4ef440b86319c80e1a0a5f63d6db54836f390
Yakzan Code <yakzancode@outlook.com>
2023-09-02 11:44:51 +0200

    ingredien't images added

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const multer = require('multer');
+const path = require('path');
 
-app.use(cors({ origin: 'https://your-main-application-domain.com' }));
+const app = express();
+app.use(cors({ origin: 'https://localhost:4200' }));
 
 app.use(express.static('images'));
 
+const storage = multer.diskStorage({
+  destination: 'images',
+  filename: (req, file, callback) => {
+    callback(null, Date.now() + '-' + file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+app.post('/upload', upload.single('image'), (req, res) => {
+  res.status(200).json({ message: 'File uploaded successfully' });
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Image server is running on port ${PORT}`);
+  console.log(`Image server is running on port ${PORT}`);
 });
